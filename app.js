53448d26587a032270ed79b197d9f4f53a0ed88a
Doncho Minkov <donchominkov@gmail.com>
2014-03-05 09:53:25 +0200

    added core

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
+  app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
 });
 
 app.configure('development', function(){
