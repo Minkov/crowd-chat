c30c9bd5b2d96111434e1aee0e745786c7becd5d
Doncho Minkov <donchominkov@gmail.com>
2014-03-06 17:52:23 +0200

    Added cors - take 5

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var express = require('express'),
 
 var app = express();
 
-var enableCors = function(req, res, next){
+var enableCors = function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -31,7 +31,32 @@ app.configure(function() {
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 
-  app.use(enableCors);
+  // app.use(enableCors);
+  app.use(function(req, res, next) {
+    var oneof = false;
+    if (req.headers.origin) {
+      res.header('Access-Control-Allow-Origin', req.headers.origin);
+      oneof = true;
+    }
+    if (req.headers['access-control-request-method']) {
+      res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+      oneof = true;
+    }
+    if (req.headers['access-control-request-headers']) {
+      res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+      oneof = true;
+    }
+    if (oneof) {
+      res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+    }
+
+    // intercept OPTIONS method
+    if (oneof && req.method == 'OPTIONS') {
+      res.send(200);
+    } else {
+      next();
+    }
+  });
   // app.use(function(req, res, next) {
   //   res.header("Access-Control-Allow-Origin", "*");
   //   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
