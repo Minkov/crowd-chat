bf9e4f973e3c3b1ebd1f2dd588a3f176f20959f1
Doncho Minkov <donchominkov@gmail.com>
2014-03-06 17:32:06 +0200

    Added cors - take 2

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ var express = require('express'),
 var app = express();
 
 var enableCors = function(req, res, next){
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   if('OPTIONS' === req.method){
     res.send(200);
   }
