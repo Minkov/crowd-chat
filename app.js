e0117c34e0a5635112d6420b964fe6533bdd9d11
Doncho Minkov <donchominkov@gmail.com>
2014-03-06 18:19:06 +0200

    Cors enabled

--- app.js
+++ app.js
@@ -9,17 +9,43 @@ var express = require('express'),
 
 var app = express();
 
+// var enableCors = function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   next();
+//   // if('OPTIONS' === req.method){
+//   //   res.send(200);
+//   // }
+//   // else{
+//   //   next();
+//   // }
+// };
+
 var enableCors = function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-  // if('OPTIONS' === req.method){
-  //   res.send(200);
-  // }
-  // else{
-  //   next();
-  // }
+  console.log("Cors handler invoked");
+  var oneof = false;
+  if (req.headers.origin) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    oneof = true;
+  }
+  if (req.headers['access-control-request-method']) {
+    res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+    oneof = true;
+  }
+  if (req.headers['access-control-request-headers']) {
+    res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+    oneof = true;
+  }
+  if (oneof) {
+    res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+  }
+  // intercept OPTIONS method
+  if (oneof && req.method == 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
 };
 
 app.configure(function() {
@@ -28,40 +54,11 @@ app.configure(function() {
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(enableCors);
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 
   // app.use(enableCors);
-  app.use(function(req, res, next) {
-    var oneof = false;
-    if (req.headers.origin) {
-      res.header('Access-Control-Allow-Origin', req.headers.origin);
-      oneof = true;
-    }
-    if (req.headers['access-control-request-method']) {
-      res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
-      oneof = true;
-    }
-    if (req.headers['access-control-request-headers']) {
-      res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
-      oneof = true;
-    }
-    if (oneof) {
-      res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
-    }
-
-    // intercept OPTIONS method
-    if (oneof && req.method == 'OPTIONS') {
-      res.send(200);
-    } else {
-      next();
-    }
-  });
-  // app.use(function(req, res, next) {
-  //   res.header("Access-Control-Allow-Origin", "*");
-  //   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  //   next();
-  // });
 });
 
 app.configure('development', function() {
