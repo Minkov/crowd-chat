a930bd0139141e6bff42fba1915f6cf39020645e
Doncho Minkov <donchominkov@gmail.com>
2014-03-06 17:08:51 +0200

    Added mongodb

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path');
+var express = require('express'),
+  routes = require('./routes'),
+  http = require('http'),
+  path = require('path');
 
 var app = express();
 
-app.configure(function(){
+app.configure(function() {
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -20,7 +19,7 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
-/*
+  /*
   app.use(function(req, res, next){
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -29,7 +28,7 @@ app.configure(function(){
 */
 });
 
-app.configure('development', function(){
+app.configure('development', function() {
   app.use(express.errorHandler());
 });
 
@@ -42,8 +41,8 @@ app.all('*', function(req, res, next) {
 
 app.get('/posts', routes.getPosts);
 app.post('/posts', routes.addPost);
-app.get('/posts/:index', routes.getPostsSince);
+app.get('/posts/:id', routes.getPostsSince);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function() {
   console.log("Express server listening on port " + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -1,91 +1,115 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/crowd-chat');
 
-/*
- * GET home page.
- */
+var MessageSchema = new mongoose.Schema({
+	by: String,
+	text: String,
+	sentDate: Date
+});
 
-/*
-var posts =  posts || [{
-	index: 0,
-	text: "Sample 1",
-	user: "Doncho Minkov",
-	date: "04-03-2014 17:40:00"
-}, {
-	index: 1,
-	text: "Sample 2",
-	user: "Doncho Minkov",
-	date: "04-03-2014 17:40:00"
-}];
-*/
+mongoose.model('Message', MessageSchema);
 
-var posts = [],
-	maxPostIndex = 10000000,
-	maxPostsCount = 10000;
+var Message = mongoose.model('Message');
 
-function checkPostsCount(){
-	if(posts.length >= maxPostsCount){
+function checkPostsCount() {
+	if (posts.length >= maxPostsCount) {
 		posts = posts.slice(posts.length - maxPostsCount);
 	}
 }
 
-function checkPostsIndices(){
-	if(posts.length){
-		if(posts[posts.length-1].index >= maxPostIndex){
-			for(var i = 0; i < posts.length; i+=1){
+function checkPostsIndices() {
+	if (posts.length) {
+		if (posts[posts.length - 1].index >= maxPostIndex) {
+			for (var i = 0; i < posts.length; i += 1) {
 				posts[i].index = i;
 			}
 		}
 	}
 }
 
-if(!String.prototype.htmlEscape){
+if (!String.prototype.htmlEscape) {
 	String.prototype.htmlEscape = function() {
-		 return this.replace(/&/g, '&amp;')
-					.replace(/</g, '&lt;')
-					.replace(/>/g, '&gt;')
-					.replace(/"/g, '&quot;')
-					.replace(/'/g, "&#39")
-					.replace(/ /g,"&nbsp;");
+		return this.replace(/&/g, '&amp;')
+			.replace(/</g, '&lt;')
+			.replace(/>/g, '&gt;')
+			.replace(/"/g, '&quot;')
+			.replace(/'/g, "&#39")
+			.replace(/ /g, "&nbsp;");
 	};
 }
 
-exports.getPosts = function(req, res){
-	checkPostsCount();
-	checkPostsIndices();
-	res.json(posts);
+exports.getPosts = function(req, res) {
+	Message.find().exec()
+		.then(function(messages) {
+			console.log(messages);
+			if (messages && messages.length) {
+				res.json(messages);
+			} else {
+				res.json([]);
+			}
+		});
 };
 
-exports.getPostsSince = function(req, res){
-	checkPostsCount();
-	checkPostsIndices();
-	if(!req.params.hasOwnProperty("index")){
+exports.getPostsSince = function(req, res) {
+	if (!req.params.hasOwnProperty('id')) {
 		var errResponse = {
 			message: "Invalid post index",
 			err_code: "INV_POST"
 		};
 		return res.json(errResponse);
 	}
+	var date;
+	Message.find({
+		'_id': req.params.id
+	}).exec()
+		.then(function(messages) {
+			console.log('Here!');
+			if (!messages.length) {
+				res.json([]);
+			}
+			date = messages[0].get('sentDate');
+			console.log(date);
+			return Message.find().exec();
+		})
+		.then(function(messages) {
+			var laterMessages = [];
+			for (var i = 0; i < messages.length; i += 1) {
+				if (messages[i].get('sentDate') > date) {
+					laterMessages.push(messages[i]);
+				}
+			}
+			res.json(laterMessages);
+		});
+};
 
-	var index = parseInt(req.params.index);
-	if(index<0 || index>posts.length) index = 0;
-	res.json(posts.slice(index));
-}
-
-exports.addPost = function(req, res){
-	if(!req.body.hasOwnProperty("user") ||
-	 	!req.body.hasOwnProperty("text")){
+exports.addPost = function(req, res) {
+	if (!req.body.hasOwnProperty("user") || !req.body.hasOwnProperty("text")) {
 		var errResponse = {
-			message: "Invalid post",
+			message: "Invalid message",
 			err_code: "INV_POST"
 		};
 		return res.json(errResponse);
 	}
+	var message = new Message({
+		by: req.body.user.htmlEscape(),
+		text: req.body.text.htmlEscape(),
+		sentDate: new Date()
+	});
+	message.save(function(err, msg) {
+		console.log(arguments);
+		if (err) {
+			res.status(400);
+			return res.json(err);
+		}
+		res.json(msg);
+	});
 
-	var post = {
-		index: posts.length,
-		username: req.body.user.htmlEscape(),
-		text: req.body.text.htmlEscape()
-	};
+	// var post = {
+	// index: posts.length,
+	// username: req.body.user.htmlEscape(),
+	// text: req.body.text.htmlEscape()
+	// };
 
-	posts.push(post);
-	res.json(true);
-}
+	// posts.push(post);
+	// res.json(true);
+};
