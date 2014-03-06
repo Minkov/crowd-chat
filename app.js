5e743fcb2ecf3926c4cc463ef6a1a27cfb53669c
Doncho Minkov <donchominkov@gmail.com>
2014-03-06 17:47:43 +0200

    Added cors - take 4

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var enableCors = function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
   if('OPTIONS' === req.method){
     res.send(200);
   }
