c9b41b7b2be9491d2a6bef4cc5f79ccabf9974c5
Doncho Minkov <donchominkov@gmail.com>
2014-03-06 17:49:43 +0200

    Added cors - take 4

--- app.js
+++ app.js
@@ -13,18 +13,17 @@ var enableCors = function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  if('OPTIONS' === req.method){
-    res.send(200);
-  }
-  else{
-    next();
-  }
+  next();
+  // if('OPTIONS' === req.method){
+  //   res.send(200);
+  // }
+  // else{
+  //   next();
+  // }
 };
 
 app.configure(function() {
   app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
